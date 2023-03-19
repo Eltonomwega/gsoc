8aa983d250f6a3cd0c6ee9eec235f1d779b58980
elton <ltomwega@gmail.com>
2023-03-19 14:06:25 +0300

    Feature: Wagtail tutorial

---
+++ 2023/elton-omwega/Wagtail-tutorial/mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-s5+-(gz!@gt=6@3&dy4*fd2s)n5jj7873k)=n#46ql0_5!h^1r"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
